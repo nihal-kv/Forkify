3a93aa02c9885775cc275a0c86310b3825afc4da
Nihal Singh <nks.allen@gmail.com>
2023-07-08 10:56:43 +0530

    initialCommit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '87e7873e-cf5c-473d-a6b4-f554b856dbc3';
+export const MODAL_CLOSE_SEC = 2.5;
